f02a49cd428e7c70c103d9ba6382404876c0876e
Philippe Berra <berraphilippe07@gmail.com>
2023-06-05 14:10:59 +0200

    B2

--- src/challenges/B2/answer.ts
+++ src/challenges/B2/answer.ts
@@ -7,11 +7,49 @@
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
+
 export default function ({ events }: { events: EventWithCategory[] }): string[] {
-    return [];
+    // Créer un ensemble pour stocker les catégories uniques
+    const categoriesSet = new Set<string>();
+
+    // Parcourir tous les événements et ajouter les catégories dans l'ensemble
+    events.forEach(event => {
+        event.categories.forEach(category => {
+            categoriesSet.add(category);
+        });
+    });
+
+    // Convertir l'ensemble en tableau et le trier par ordre alphabétique
+    const categoriesArray = Array.from(categoriesSet).sort();
+
+    // Retourner le tableau des catégories triées
+    return categoriesArray;
 }
-*/
+
+//OU
+
+
+/*export default function ({
+  events,
+}: {
+  events: EventWithCategory[];
+}): string[] {
+  // const categories: string[] = []
+  const categories: Set<string> = new Set();
+
+  events.forEach((event) => {
+    event.categories.forEach((category) => {
+      categories.add(category);
+    });
+  });
+  return Array.from(categories).sort();
+  //   events.forEach((event)=>{
+  //     categories.push(...event.categories)
+}
+//   })
+//   const sortedUniqueCategories = [...new Set(categories)].sort()
+//   return sortedUniqueCategories;
+// }*/ 
 
 // used interfaces, do not touch
 export interface EventWithCategory {
